4e6609d812dd2564e011e6845ed1d9f45a414308
NSFatZJUT <121429222+NSFatZJUT@users.noreply.github.com>
2026-01-04 23:56:47 +0800

    Update __init__.py

--- nlp_pyramidal_flow/embeddings/__init__.py
+++ nlp_pyramidal_flow/embeddings/__init__.py
@@ -1,1 +1,11 @@
+from nlp_pyramidal_flow.tasks.embeddings import TfidfEmbedder
+
+try:
+    from nlp_pyramidal_flow.embeddings.transformer_embedder import TransformerEmbedder  # noqa: F401
+except Exception:
+    TransformerEmbedder = None  # type: ignore
+
+__all__ = ["TfidfEmbedder", "TransformerEmbedder"]
+
+
 
